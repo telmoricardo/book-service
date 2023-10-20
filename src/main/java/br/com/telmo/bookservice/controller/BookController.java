bcc6427e4d65a8dc5ca555e8d531b5c6d7c4adfb
Telmo Ricardo <robinette13@theavyk.com>
2023-10-20 14:18:31 -0300

    add openapi docs

--- src/main/java/br/com/telmo/bookservice/controller/BookController.java
+++ src/main/java/br/com/telmo/bookservice/controller/BookController.java
@@ -3,13 +3,15 @@ package br.com.telmo.bookservice.controller;
 import br.com.telmo.bookservice.model.Book;
 import br.com.telmo.bookservice.proxy.CambioProxy;
 import br.com.telmo.bookservice.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -21,6 +23,8 @@ public class BookController {
     private BookService service;
     @Autowired
     private CambioProxy proxy;
+
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
             @PathVariable("id") Long id,
