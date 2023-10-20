56d9d21ad8c16a6eb6271cc67fbe49a9ea5844a3
Telmo Ricardo <robinette13@theavyk.com>
2023-10-20 12:52:35 -0300

    bulkhead

--- src/main/java/br/com/telmo/bookservice/controller/FooBarController.java
+++ src/main/java/br/com/telmo/bookservice/controller/FooBarController.java
@@ -1,5 +1,6 @@
 package br.com.telmo.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -18,7 +19,8 @@ public class FooBarController {
     @GetMapping("/foo-bar")
 //    @Retry(name="foo-bar", fallbackMethod = "fallbackMethod")
 //    @CircuitBreaker(name="default", fallbackMethod = "fallbackMethod")
-    @RateLimiter(name="default")
+//    @RateLimiter(name="default")
+    @Bulkhead(name="default")
     public String foobar() {
         logger.info("Request to foo-bar is receveid");
 //        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
